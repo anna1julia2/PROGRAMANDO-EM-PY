7c43a88e8025fcc479f0c7de5ed88343acdb6b46
juju <aj.ncorrea@gmail.com>
2025-05-22 17:41:52 -0300

    Update atividade 1.py annaju.py
    começo no final = calculadora

--- atividade 1.py annaju.py
+++ atividade 1.py annaju.py
@@ -1,4 +1,12 @@
+#soma de 2 fluxograma
+# 4 funções basicas
+n1 = float (input('numero 1:'))
+n2 = float (input('numero 2:'))
 
+print ('resultado soma' , n1 + n2)
+print ('resultado subtração' , n1 - n2)
+print ('resultado multiplicação', n1 * n2)
+print ('resultado divisão', n1 / n2)
 
 #comentário
 #dados primitivos
