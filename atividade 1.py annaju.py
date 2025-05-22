bec5a8c4aa57197ec52089d55be13b5bb3fc9675
juju <aj.ncorrea@gmail.com>
2025-05-22 17:31:55 -0300

    Add files via upload

---
+++ atividade 1.py annaju.py
@@ -0,0 +1,46 @@
+
+
+#comentário
+#dados primitivos
+#para carregar o código, clique em f5
+#strings = textos (str)
+print ('teste')
+
+print ('anna julia')
+print ('ok')       
+print ('oii' , 'tudo bem')
+
+#dado do tipo inteiro (int)
+print (1)
+print (-30)
+print (0)
+
+#dado do tipo float (float)
+print (1.5)
+print (10.0)
+print (30.88)
+print (.9)
+print (4.)
+
+#dado do tipo boleano (bool)
+print (True)
+print (False)
+
+
+#input () = guardar um dado
+input ('o meu nome:')
+print(input ('o meu nome:'))
+
+#  variaveis:
+
+nome = 'ANNA JULIA'
+idade= 21
+
+print ('juju' , nome)
+print ('idade', idade)
+
+#calculadora de soma
+numero_1 = int( input ('digite um numero'))
+numero_2 = int(input ('outro numero'))
+print (numero_1 + numero_2)
+
