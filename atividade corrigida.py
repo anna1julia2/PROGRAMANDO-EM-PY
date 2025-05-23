f233b8bd52daaf7e1cec591e34966d83081ad698
juju <aj.ncorrea@gmail.com>
2025-05-23 17:27:07 -0300

    Add files via upload

---
+++ atividade corrigida.py
@@ -0,0 +1,30 @@
+#1 - Crie um programa para efetuar a leitura de um número inteiro e apresentar o resultado do quadrado deste número.
+n1  =  int(input('5>4'))
+calc = n1 ** 2
+print(calc)
+
+# 2 - Crie duas variáveis para armazenar seu primeiro nome e sobrenome. Em seguida, concatene-as para formar seu nome completo e exiba o resultado.
+nome  =  'Anna'
+sobrenome  =  'Correa'
+print(nome, sobrenome)
+
+#3 - Peça ao usuário para digitar dois números inteiros e armazene-os em variáveis. Realize a concatenação desses números como strings e exiba o resultado.
+n1 = int(input('1>>2'))
+n2 = int(input('3>>4'))
+n1str = str(n2)
+n2str = str(n2)
+print(n1str, n2str)
+
+# 4 - Crie uma variável para armazenar a palavra "Python". Em seguida, adicione um número inteiro ao final da palavra usando a concatenação e exiba o resultado.
+palavra  =  'Python'
+n1 + int(input(
+print('palavra', 10)
+
+# 5 - Declare uma variável contendo uma frase. Em seguida, peça ao usuário para digitar uma palavra e concatene essa palavra no final da frase. Exiba o resultado. 
+frase = 'oi juju: '
+palavra =  input('Digite seu apelido:  ')
+concate = frase + " " + palavra
+print(concate)
+
+
+
