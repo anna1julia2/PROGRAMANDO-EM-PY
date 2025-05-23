bc33e13a9ac3be850b88d1fb77b8167ce2af832b
juju <aj.ncorrea@gmail.com>
2025-05-23 17:38:29 -0300

    Add files via upload

---
+++ aula 5 explicação.py
@@ -0,0 +1,9 @@
+#CONCATENAR -> JUNTAR
+
+#1
+nome = 'julia'
+print('bom dia', nome)
+print('bom dia' ' ' + nome)
+print(f'bom dia {nome} kkk')
+print('bom dia {}' .format (nome))
+print('bom dia %s'%(nome))
