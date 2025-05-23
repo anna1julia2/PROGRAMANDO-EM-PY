438081e95b7606ba1fade5deda7c7fe02f3f597e
juju <aj.ncorrea@gmail.com>
2025-05-23 17:51:46 -0300

    Add files via upload

---
+++ aula 5 atividade.py
@@ -0,0 +1,37 @@
+#1
+print('seja bem vinda!)
+#2
+verdadeiro = True
+print(type(verdadeiro))
+#3
+print (10.5 * 2.00)
+#4
+print(10/2)
+#5
+print(10-5)
+#6
+print(10//2)
+#7
+print(10.0 * 5.0 * 2.6 * 8.0)
+#8
+numero = 10
+print(numero ** 2)
+#9
+#nome idade email curso e endereço
+nome = input ('digite seu nome:')
+idade = input ('digite sua idade:')
+email = input ('digite seu email:')
+curso = input ('digite seu curso:')
+endereço = input ('digite seu endereço:')
+
+print(f'''
+nome: {nome}
+idade: {idade}
+email: {email}
+curso: {curso}
+endereço: {endereço}
+''')
+
+
+
+      
