887fdd8cd15675bb2a09d9b211e143f6b4f37236
juju <aj.ncorrea@gmail.com>
2025-05-29 15:57:45 -0300

    Add files via upload

---
+++ AULA7-py.py
@@ -0,0 +1,33 @@
+lista  =  [1,2,3,6]
+
+# append(1  dado) extended([varios]) +=(varios) insert(indice,dado) # adicionar
+
+# del lista[indice] remove(valor) pop(indice) pop() deletar o ultimo #deletar
+
+# len() sum() min() max() copy() clear() # verificar
+tamanho = len(lista)
+print(tamanho)
+
+# sum - somar
+lista  =  [1,2,3,6]
+somar = sum(lista)
+print(somar)
+
+menor  =  min(lista)
+print(menor)
+
+maior  =  max(lista)
+print(maior)
+
+copia = lista.copy()
+print(copia)
+
+lista.clear()
+print(lista)
+
+
+m = 'hello ju'
+m = m.split()
+print(m)
+
+#append add um dado por vez
