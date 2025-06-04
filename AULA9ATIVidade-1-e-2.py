efb6b9729b154e9c0900f06557db1e119928ca77
juju <aj.ncorrea@gmail.com>
2025-06-04 17:46:53 -0300

    Add files via upload

---
+++ AULA9ATIVidade-1-e-2.py
@@ -0,0 +1,46 @@
+#c = 0
+WHILE C < 1001:
+    #PRINT('TESTE' , C)
+    #c = c =1
+    
+    
+nomes = []
+contador = 0
+while contador <=10:
+    nome = input('digite o nome da pessoa {contador =1}')
+    nomes.append(nome)
+    contador += 1
+    
+    print('lista de nomes:')
+for nome in nomes:
+    print(nome)
+    
+    
+    
+senha = '1234'
+acesso = 'ju'
+p = 0
+while p <= 3
+     senha_user = input('digite sua senha:')
+     login = input('digite seu login')
+     p = p +1
+     if senha == senha_user and login == acesso:
+         print('sistemas de notas')
+         quantas_notas = int(input('quantidade notas:'))
+         for n in range(quantas_notas):
+            nota =  float(input('Digita a nota: '))
+            lista_notas.append(nota)
+            soma = sum(lista_notas)
+            media  =  soma / quantas_notas
+        print('Media - ', media)
+        break
+             
+        
+        
+    else:
+        print('Digitação incorreta! ')
+else:
+    print('Conta bloqueada!!')
+ 
+
+
