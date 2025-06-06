9cf88a923d321627b668e6272c3a1c8d530fe9bb
juju <aj.ncorrea@gmail.com>
2025-06-06 17:56:28 -0300

    Add files via upload

---
+++ AULA12-atividades.py
@@ -0,0 +1,95 @@
+#atividade 1
+
+import random
+
+aleatorio1 = random.randint(5,10)
+aleatorio2 = random.randint(5,10)
+aleatorio3 = random.randint(5,10)
+
+
+print(f'''
+{aleatorio1}
+{aleatorio2}
+{aleatorio3}
+''')
+
+# atividade 2
+
+import random
+
+aleatorio4 = random.randint(8)
+aleatorio5 = random.randint(9)
+aleatorio6 = random.randint(6)
+
+print(f'''
+{aleatorio8}
+{aleatorio9}
+{aleatorio6}
+''')
+
+# atividade 3
+
+import range
+
+aleatorio5 = random.range(10,30)
+aleatorio6 = random.range(10,30)
+aleatorio7 = random.range(10,30)
+
+print(f'''
+{aleatorio8}
+{aleatorio9}
+{aleatorio6}
+''')
+
+#4
+def atividade_4():
+for i in range(10,0,-1);
+    print(i)
+    print('fogo!')
+    
+    
+#5    
+def atividade_5():
+    n = int(input('digite um numero interio'))
+    i = []
+    for i in range (0,n,2):
+	    1.append(i)
+	    soma= sum(1)
+	print(soma)
+	
+	
+#6
+def atividade_6():
+	valor_n = int(input('=='))
+	for n in range(10):
+		calculo = valor_n * n
+		print(valor_n, 'x', n, '=', calculo)
+		
+atividade_6()
+
+#7
+s = 0
+def atividade_7():
+	for n in range(99,0,-2):
+		if n % 2 == 1:
+		print(n)
+		
+		
+		
+	
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
