533cc07376d72e39ccb4a697b77d9c3f12927a9e
juju <aj.ncorrea@gmail.com>
2025-06-11 11:40:00 -0300

    Add files via upload

---
+++ F-D-P-aula2.py
@@ -0,0 +1,27 @@
+#\    |    /
+# \n - pular linha
+#chamando alguma função.
+#sep=
+#end= para printar tudo em uma linha só
+#print("python", "é", sep="->", "incrivel", end="!", "vamos", "programar", sep="->", "mais", end="!")
+#variavel= dado+texto
+#interio = 10 = int
+# decimal = 3.13 = float
+#teste = 'ju' = string = str
+#verdadeiro = True = bool = boleano
+#type = informa a variavael que nao sabe qual esta chamando
+# # = comentario
+#len = conta os caracteres da frases
+#input = recebe qualquer inf que for digitar
+#-----------------------------------------------------------------------------------------
+#print(type(intero))
+#print(type(dacimal))
+#print(type(verdadeiro))
+#print(type(texto))
+#-----------------------------------------------------------------------------------------
+#concatenar = juntar
+#regra matematica = ( 6 + 10 / 2) 11.0 >>> resolver primeiro a divisao dps a soma
+#((6 + 10) / 2) 8.0 >>> resolver primeiro os parenteses depois a divisao
+#print(frase.lower()) de maiuscula > minuscula
+#print(frase.upper()) de minuscula > maiuscula
+#substituir = print(frase.replace('mundo', 'python') ola python (ola mundo)
