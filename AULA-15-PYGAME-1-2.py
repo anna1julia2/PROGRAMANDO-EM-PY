06d1f1bc5424b814a9376213cdb1df94932bde0c
juju <aj.ncorrea@gmail.com>
2025-06-11 17:55:18 -0300

    Add files via upload

---
+++ AULA-15-PYGAME-1-2.py
@@ -0,0 +1,10 @@
+#pip
+#SQlite 3 - tkinter
+#pygame
+#variavel e modulo
+#rgb - vermelho verde e azul
+#cores - fiil - preencher
+#obrigada a criar o lupin pq nao tem essa funçao
+#while true - lupin infinito
+#init = iniciar
+#nome do modulo = display
