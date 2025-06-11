b8a742ce204e758e325a1eab370970943428f7f7
juju <aj.ncorrea@gmail.com>
2025-06-11 15:45:29 -0300

    Add files via upload

---
+++ AULA14-match-case.py
@@ -0,0 +1,9 @@
+#match = analise // case = caso // 'se' !! ou ??
+#if tambem -SE-
+#condicoes rapidas, so consegue analisar oq tem dentro. limitado
+#>case _:< caso nao tenha a opção colocar a opção 'nao existe'
+#------------------------------------
+#VS CODE PARA SALVAR 9ctrl S)
+#-------------------------------------
+#dado logico = string vazia
+# selecionar e CTRL e 2 . (:) coloca # automaticamente
